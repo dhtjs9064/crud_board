10f969e5391fddec2b41bbae73c74527a8406d98
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2025-09-23 15:48:44 +0900

    feat : boardID 구현내용이 중복되므로 다른 클래스로 구현함
    이 클래스를 통해 boardId검사를 손쉽게 할 수 있다.

---
+++ src/main/java/com/study/connection/BoardUtils.java
@@ -0,0 +1,40 @@
+package com.study.connection;
+
+import com.study.connection.dao.BoardDAO;
+import com.study.connection.dto.BoardDTO;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BoardUtils {
+
+    public static BoardDTO getValidBoard(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO: 아래의 검증 과정은 중복일까 아닐까
+        // TODO: 에러일경우 각각 어떻게 처리할건지, 메시지는 어떻게 보여줄것인지
+        String boardIDParam = request.getParameter("boardID");
+
+        int boardID;
+        try {
+            boardID = Integer.parseInt(boardIDParam);
+            // 숫자가 아니면 에러
+        } catch (NumberFormatException e) {
+            request.setAttribute("errorMessage", "유효하지 않은 글입니다.");
+            return null;
+        }
+
+        // 컨트롤러에서 dao의 getBoard()를 통해 boardID 값을 가져옴
+        BoardDAO dao = new BoardDAO();
+        // getBoard의 반환 타입 = BoardDTO
+        BoardDTO board = dao.getBoard(boardID);
+
+        // DB에 저장되어 있지 않으면 에러
+        if (board == null) {
+            request.setAttribute("errorMessage", "삭제된 글입니다.");
+            return null;
+        }
+
+        return board;
+    }
+}
