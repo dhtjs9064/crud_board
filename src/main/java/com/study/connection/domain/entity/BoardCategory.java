963f12483fdbe1db24969dc53dbe7b1d3d6021f6
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2025-10-02 15:19:01 +0900

    feat : 카테고리 entity 생성
    카테고리 db 및 entity를 생성하여 테이블을 분리함

---
+++ src/main/java/com/study/connection/domain/entity/BoardCategory.java
@@ -0,0 +1,9 @@
+package com.study.connection.domain.entity;
+
+import lombok.Getter;
+
+@Getter
+public class BoardCategory {
+    private String CategoryId; // 카테고리 기본키
+    private String CategoryName; // 카테고리 이름
+}
